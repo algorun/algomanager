cec963855c822b4d4092849e6897bf1f932f89af
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-08-10 11:15:54 -0400

    first version for the API

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # api
 The API used to perform the underlying container management of algopiper
+
+## HTTP POST /run
+### Parameters
+- **image**: the name of the docker image to run
+### Results
+If the docker run is successfull, it returns JSON object in the following format:
+```
+{
+**'status'**: 'success',
+**'contianer_id'**: <the-container-id-returned-by-docker-daemon>,
+**'endpoint'**: <hostname:portnumber>
+}
+```
+If the docker run is unsuccessfull, it returns JSON object in the following format:
+```
+{
+**'status'**: 'fail',
+**'error_message'**: <the-stderr-returned-by-docker-daemon>
+}
+```
+
+## HTTP POST /stop
+### Parameters
+- **container_id**: the id of the container *that you previously received from the run endpoint*
+### Results
+If docker stop is successfull, it returns JSON object in the following format:
+```
+{
+**'status'**: 'success',
+**'contianer_id'**: <the-stopped-contianer-id-returned-by-docker-daemon>
+}
+```
+If the docker stop is unsuccessfull, it returns JSON object in the following format:
+```
+{
+**'status'**: 'fail',
+**'error_message'**: <the-stderr-returned-by-docker-daemon>
+}
+```
---
+++ Server.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var fs = require('fs');
+var path = require('path');
+var exec = require("child_process").exec;
+
+var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+
+var available_ports = [];
+var start_port = 49152;
+var end_port = 65535;
+for(var i = start_port; i<= end_port; i++) {
+    available_ports.push(i);
+}
+
+var running_containers = {};
+
+app.post('/run', function (req, res) {
+    var docker_image = req.body.image;
+    var container_port = available_ports.shift();
+    var run_command = 'docker run -d -p ' + container_port + ':8765 ' + docker_image;
+    exec(run_command, function (err, stdout, stderr) {
+        var run_result = {};
+        if (!err){
+            run_result['status'] = 'success';
+            run_result['container_id'] = stdout.trim();
+            run_result['endpoint'] = 'http://' + server.address().address + ':' + container_port;
+            
+            // save the container port number for future remove
+            running_containers[stdout.trim()] = container_port;
+            
+            res.status = 200;
+            res.send(run_result);
+        } else {
+            run_result['status'] = 'fail';
+            run_result['error_message'] = stderr.trim();
+            
+            // return the port back to the pool
+            available_ports.push(container_port);
+            
+            res.status = 200;
+            res.send(run_result);
+        }
+    });
+});
+
+app.post('/stop', function (req, res) {
+    var container_id = req.body.container_id;
+    var stop_command = 'docker stop ' + container_id;
+    exec(stop_command, function (err, stdout, stderr) {
+        var stop_result = {};
+        if (!err){
+            stop_result['status'] = 'success';
+            stop_result['container_id'] = stdout.trim();
+            
+            // return the port back to the pool
+            available_ports.push(running_containers[container_id]);
+            delete running_containers[container_id];
+            
+            res.status = 200;
+            res.send(stop_result);
+        } else {
+            stop_result['status'] = 'fail';
+            stop_result['error_message'] = stderr.trim();
+            
+            res.status = 200;
+            res.send(stop_result);
+        }
+    });
+});
+
+app.use(express.static(__dirname));
+
+var server = app.listen(8764, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('API server listening at http://%s:%s ..', host, port);
+
+});

292ac79f5208464b0506eed4c179ae9bce56a8ea
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-08-25 20:59:10 -0400

    fully functional garbage collector

--- Server.js
+++ Server.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var Docker = require('dockerode');
 var enableDestroy = require('./CloseServer.js');
+var CronJob = require('cron').CronJob;
+var request = require('request');
+
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
 
 var host = '';
@@ -24,6 +27,7 @@ for(var i = start_port; i<= end_port; i++) {
     available_ports.push(i);
 }
 var running_containers = [];
+var stop_after = 8*60*60*1000;   // the idle time after which to stop a running container
 
 app.get('/api/v1/status', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -117,6 +121,7 @@ process.on('SIGINT', function () {
             console.log("container " + running_containers[i]["container_id"] + " stopped .. ");
         }
     }
+    gc.stop();
     server.destroy();
 });
 
@@ -131,3 +136,53 @@ var server = app.listen(8764, function () {
 
 });
 enableDestroy(server);
+
+// definition for the garbage collector
+// run every hour
+var cron_expression = '0 0 * * * *';
+var gc = new CronJob(cron_expression, function(){
+    var now = new Date();
+    // loop through running containers to stop the ones that have more than X hours being idle
+    console.log(running_containers);
+    if(running_containers.length !== 0){
+        running_containers.forEach(function(acontainer, index){
+            (function(c, i){
+                var endpoint = "http://x.algorun.org:" + c["container_port"] + "/v1/status";
+                request(endpoint, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var continaer_status = JSON.parse(body);
+                        var running_since;
+                        if(continaer_status["last_used"] !== 'never') {
+                            running_since = now - (new Date(continaer_status["last_used"]));
+                            if(running_since >= stop_after){
+                                // stop this container now and remove it from the running list
+                                docker.getContainer(c["container_id"]).stop(function(error, response, body){});
+                                console.log("container " + c["container_id"] + " stopped .. ");
+                                // return the port back
+                                available_ports.push(c["container_port"]);
+                                // remove it from running containers
+                                var index_to_remove = running_containers.indexOf(c);
+                                running_containers.splice(index_to_remove, 1);
+                            }
+                        } else {
+                            // the container is never used. check its creation time
+                            created_since = now - (new Date(c["time_created"]));
+                            if(created_since >= stop_after){
+                                // stop this container now and remove it from the running list
+                                docker.getContainer(c["container_id"]).stop(function(error, response, body){});
+                                console.log("container " + c["container_id"] + " stopped .. ");
+                                // return the port back
+                                available_ports.push(c["container_port"]);
+                                // remove it from running containers
+                                var index_to_remove = running_containers.indexOf(c);
+                                running_containers.splice(index_to_remove, 1);
+                            }
+                        }
+                    } else {
+                        console.error("Error getting container " + running_containers[i]["container_id"] + " status!");
+                    }
+                });
+            }(acontainer, index));
+        });
+    }
+}, null, true, "America/New_York");

2b2b8db65602162496b74925bdce8a57d2517102
Abdelrahman <abdelrahman.hosny@hotmail.com>
2016-02-02 21:57:17 -0500

    added cron job for stopping containers

--- manager.js
+++ manager.js
@@ -6,6 +6,7 @@ var enableDestroy = require('./CloseServer.js');
 var CronJob = require('cron').CronJob;
 var request = require('request');
 var nmap = require('libnmap');
+var forever = require('forever');
 var opts = {range: ['localhost'], ports: '10000-60000'}; // allow docker containers to be run on this port range
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
 var server_path = 'http://localhost:';
@@ -47,6 +48,16 @@ function getRandomAvailablePort(callback){
     });    
 }
 
+function cleanup(){
+    // stop all running AlgoManager containers
+        running_containers.forEach(function(acontainer, index){
+            (function(c, i){
+                docker.getContainer(c["container_id"]).stop(function(error, response, body){});
+                console.log("container " + c["container_id"] + " stopped .. ");
+            }(acontainer, index));
+        });
+}
+
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -60,6 +71,8 @@ var server = app.listen(8764, function () {
     
     console.log('AlgoManager server listening at http://%s:%s ..', host, port);
 });
+enableDestroy(server);
+forever.startServer(server);
 
 app.post('/api/v1/deploy', function(req, res){
     var docker_image = req.body.docker_image;
@@ -99,7 +112,7 @@ app.post('/api/v1/deploy', function(req, res){
                 }
                 
                 // save running container info
-                running_containers.push({'node_id': node_id, container_id: container.id, 'port': container_port, 'docker_image': docker_image, 'cteated': new Date()});
+                running_containers.push({'node_id': node_id, container_id: container.id, 'port': container_port, 'docker_image': docker_image, 'created': new Date()});
                 res.status = 200;
                 res.send({"status": 'success', "endpoint": server_path + container_port});
                 return;
@@ -107,18 +120,27 @@ app.post('/api/v1/deploy', function(req, res){
         });
     });
 });
-enableDestroy(server);
-function cleanup(){
-    // stop all running AlgoManager containers
-        running_containers.forEach(function(acontainer, index){
-            (function(c, i){
-                docker.getContainer(c["container_id"]).stop(function(error, response, body){});
-                console.log("container " + c["container_id"] + " stopped .. ");
-            }(acontainer, index));
-        });
-}
+
+// definition for the garbage collector
+// run every minute
+var cron_expression = '0 * * * * *';
+var gc = new CronJob(cron_expression, function(){
+    var now = new Date();
+    var stop_after = 2 * 60 * 60 * 1000;   // the time after which to stop a running container in milliceconds (2 hours)
+    
+    // loop through running containers to stop the ones that have more than X hours being idle
+    for(var i=0;i<running_containers.length;i++){
+        var running_since = now - running_containers[i]['created'];
+        if(running_since >= stop_after){
+            docker.getContainer(running_containers[i]['container_id']).stop(function(error, response, body){});
+            running_containers.splice(i--, 1); // remove it from the running containers
+        }
+    }
+}, null, true, "America/New_York");
+
 // stop running containers on process exit
 process.on('SIGINT', function () {
     cleanup();
+    gc.stop();
     server.destroy();
 });

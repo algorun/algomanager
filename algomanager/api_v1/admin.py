9d5ba806156e432d8405cdf73b9322a114b22662
Abdelrahman <abdelrahman.hosny@hotmail.com>
2017-01-26 14:19:06 -0500

    upgrade endpoint list

--- algomanager/api_v1/admin.py
+++ algomanager/api_v1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import RunningContainer, AvailableAlgorithms
+
+admin.site.register(RunningContainer)
+admin.site.register(AvailableAlgorithms)
--- algomanager/api_v1/views.py
+++ algomanager/api_v1/views.py
@@ -1,5 +1,45 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from algorun import run_container
+from models import RunningContainer, AvailableAlgorithms
+import uuid
+import json
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the API index.")
+def launch(request):
+    docker_image = request.GET.get('docker_image', None)
+    if 'visitor' in request.COOKIES:
+        visitor = request.COOKIES['visitor']
+    else:
+        visitor = uuid.uuid4()
+
+    # check to see if there is a running container for that user with the same docker image
+    try:
+        running_container = RunningContainer.objects.get(visitor_id=visitor, docker_image=docker_image)
+    except RunningContainer.DoesNotExist:
+        running_container = None
+
+    if running_container:
+        result = {'success': True, \
+                  'response': running_container.port_number}
+    else:
+        result = run_container(docker_image, visitor)
+
+    response = HttpResponse(json.dumps(result))
+    response.set_cookie('visitor', visitor)
+
+    return response
+
+
+def list(self):
+    images = []
+    available_algorithms = AvailableAlgorithms.objects.all()
+    for algorithm in available_algorithms:
+        av_algo = {'name': algorithm.algorithm_name, \
+                   'docker': algorithm.docker_image}
+        images.append(av_algo)
+
+    result = {'images': images}
+    response = HttpResponse(json.dumps(result))
+
+    return response

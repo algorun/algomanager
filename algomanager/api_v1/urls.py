1e7d58ccea8c6cb4b24b29f429f2576d445ee136
Abdelrahman <abdelrahman.hosny@hotmail.com>
2017-01-31 17:22:59 -0500

    restructuring repo

--- algomanager/api_v1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^api/v1/list', views.list, name='list'),
-    url(r'^api/v1/deploy', views.deploy, name='deploy'),
-    url(r'^', views.home, name='home'),
-]
